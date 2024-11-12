8a6b6423553876430ab40cc25475ee851ae62728
amalreji111 <amalreji111@gmail.com>
2024-11-12 10:51:36 +0530

    Conflict fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     },
   },
 
-  base: "",
+  base: "/ai-ui-new/",
   css: {
     modules: {
       scopeBehaviour: "local",
@@ -44,13 +44,25 @@ export default defineConfig({
     // // ),
     // __PAP_ID__: undefined,
     // __APP_FRONT__: JSON.stringify("power-user"),
+ // AI-UI Dev settings
+ // __HOME_BASE__: JSON.stringify("https://ai-worker.mjtdev.workers.dev"),
+ // __PAP_ID__: JSON.stringify(
+ //   "access-point-1725571260862-23455f95-1253-4dfe-96e3-952ac6af647c"
+ // ),
+ __PAP_ID__:JSON.stringify("access-point-1731108469504-7d3dfd30-7487-4149-8b5b-24239497e20c"),
 
+ // __APP_FRONT__: JSON.stringify("pizza-demo"),
+
+ //Greeter project config
+ // __PAP_ID__:JSON.stringify("access-point-1730138425212-a96c9489-58c9-42ba-9cb4-a85e230bb408"),
+ // __PAP_ID__:JSON.stringify("access-point-1729798916406-965d156a-2242-44af-9a4a-7e38de595a1f"),
+ // __APP_FRONT__: JSON.stringify("ai-ui-chat"),
+ __APP_FRONT__: JSON.stringify("greeter-demo"),
     // AI-UI Production settings
     __HOME_BASE__: JSON.stringify("https://ai-worker.intelligage.workers.dev"),
-    __PAP_ID__: JSON.stringify(
-      "access-point-1725571260862-23455f95-1253-4dfe-96e3-952ac6af647c"
-    ),
-    __APP_FRONT__: JSON.stringify("pizza-demo"),
+    // __PAP_ID__: JSON.stringify(
+    //   "access-point-1725571260862-23455f95-1253-4dfe-96e3-952ac6af647c"
+    // ),
 
     // AI-Workfroce Production settings
     // __HOME_BASE__: JSON.stringify("https://ai-worker.intelligage.workers.dev"),
