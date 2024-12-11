b3d6129d299b0bc2ae7c2029a8657fe6bff5f935
amalreji111 <amalreji111@gmail.com>
2024-12-11 23:14:00 +0530

    Deleted unwanted greeter app

--- src/app-front/APP_FRONTS.tsx
+++ src/app-front/APP_FRONTS.tsx
@@ -3,10 +3,8 @@ import { OverlaySpa } from "../ui/overlay/OverlaySpa";
 import { PlaygroundSpa } from "../ui/playground/PlaygroundSpa";
 import { PowerUserSpa } from "../ui/Spa";
 import { PizzaDemoFront } from "./pizza-demo/PizzaDemoFront";
-import ChatInterface from "./greeter-demo/GreeterDemo";
-import { GreeterDemoFront } from "./greeter-demo/GreeterDemoFront";
+import { GreeterDemoFront } from "./xmas-greeter/GreeterDemoFront";
 import { AiUiScreen } from "./ai-ui-chat/GreeterDemoFront";
-import { BettyDemoFront } from "./greeter-demo-1/GreeterDemoFront";
 
 export const APP_FRONTS: Record<string, ReactChild> = {
   "power-user": <PowerUserSpa />,
@@ -14,6 +12,5 @@ export const APP_FRONTS: Record<string, ReactChild> = {
   overlay: <OverlaySpa />,
   "pizza-demo": <PizzaDemoFront />,
   "xmas-demo":<GreeterDemoFront />,
-  "betty-demo":<BettyDemoFront />,
   "ai-ui-chat":<AiUiScreen/>
 } as const;
