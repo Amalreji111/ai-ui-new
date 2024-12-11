2ecc9f383e09797f0331725154015b84f81a6ca3
amalreji111 <amalreji111@gmail.com>
2024-12-11 23:23:08 +0530

    Deleted unwanted greeters

--- src/app-front/APP_FRONTS.tsx
+++ src/app-front/APP_FRONTS.tsx
@@ -3,19 +3,14 @@ import { OverlaySpa } from "../ui/overlay/OverlaySpa";
 import { PlaygroundSpa } from "../ui/playground/PlaygroundSpa";
 import { PowerUserSpa } from "../ui/Spa";
 import { PizzaDemoFront } from "./pizza-demo/PizzaDemoFront";
-import ChatInterface from "./greeter-demo/GreeterDemo";
-import { GreeterDemoFront } from "./greeter-demo/GreeterDemoFront";
 import { AiUiScreen } from "./ai-ui-chat/GreeterDemoFront";
-import { BettyDemoFront } from "./greeter-demo-1/GreeterDemoFront";
-import { CircularGreeterDemoFront } from "./greeter-demo-2/GreeterDemoFront";
+import { CircularGreeterDemoFront } from "./xmas-betty-circular/GreeterDemoFront";
 
 export const APP_FRONTS: Record<string, ReactChild> = {
   "power-user": <PowerUserSpa />,
   playground: <PlaygroundSpa />,
   overlay: <OverlaySpa />,
   "pizza-demo": <PizzaDemoFront />,
-  "xmas-demo":<GreeterDemoFront />,
-  "betty-demo":<BettyDemoFront />,
   "ai-ui-chat":<AiUiScreen/>,
   "circular-greeter":<CircularGreeterDemoFront/>
 } as const;
